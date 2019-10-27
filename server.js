a9b8d4c732856c18a4fc87814e1340364019ab3a
Kristen Stroup <kristenwhitson@gmail.com>
2019-10-27 11:35:46 -0500

    add handlebar views, css, google font and bootstrap links

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ var axios = require("axios");
 // Initialize Express
 var app = express();
 
+// // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+// var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// mongoose.connect(MONGODB_URI);
+
 // Database configuration
 var databaseUrl = "scrape";
 var collections = ["scrapeData"];
@@ -41,5 +46,48 @@ app.get("/all", function (req, res) {
             res.json(found);
         }
     });
+});
+
+// Scrape data from one site and place it into the mongodb db
+app.get("/scrape", function (req, res) {
+    // Make a request via axios for the news section of `ycombinator`
+    axios.get("https://news.ycombinator.com/").then(function (response) {
+        // Load the html body from axios into cheerio
+        var $ = cheerio.load(response.data);
+        // For each element with a "title" class
+        $(".title").each(function (i, element) {
+            // Save the text and href of each link enclosed in the current element
+            var title = $(element).children("a").text();
+            var link = $(element).children("a").attr("href");
+
+            // If this found element had both a title and a link
+            if (title && link) {
+                // Insert the data in the scrapedData db
+                db.scrapeData.insert({
+                    title: title,
+                    link: link
+                },
+                    function (err, inserted) {
+                        if (err) {
+                            // Log the error if one is encountered during the query
+                            console.log(err);
+                        }
+                        else {
+                            // Otherwise, log the inserted data
+                            console.log(inserted);
+                        }
+                    });
+            }
+        });
+    });
+
+    // Send a "Scrape Complete" message to the browser
+    res.send("Scrape Complete");
+});
+
+
+// Listen on port 3000
+app.listen(3000, function () {
+    console.log("App running on port 3000!");
 });
 
